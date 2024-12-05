9d86a2b3cec082ed8525955a664074870999f7db
BillyChilly808 <147418914+BillyChilly808@users.noreply.github.com>
2024-12-05 02:49:41 -1000

    new update for project

---
+++ src/utils/helpers.js
@@ -0,0 +1,14 @@
+export function setButtonText(
+  btn,
+  isLoading,
+  defaultText = "Save",
+  loadingText = "Saving..."
+) {
+  if (isLoading) {
+    buttonElement.textContent = loadingText;
+    console.log(`Setting text to ${loadingText}`);
+  } else {
+    console.log(`Setting text to ${defaultText}`);
+    buttonElement.textContent = defaultText;
+  }
+}
